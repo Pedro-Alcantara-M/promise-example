36ac0b0cb4f04f2d8ee059a343f7fd55e9b36ee5
Pedro-Alcantara-M <pedrinrn@gmail.com>
2024-03-18 22:39:28 -0300

    feat: adjust eslint and prettier conflicts

--- build/index.js
+++ build/index.js
@@ -1,49 +1,83 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-const debug = require('debug');
-const logger = debug('core');
-const delays = [...Array(50)].map(() => Math.floor(Math.random() * 900) + 100);
-const load = delays.map((delay) => () => new Promise((resolve) => {
-    setTimeout(() => resolve(Math.floor(delay / 100)), delay);
-}));
-const throttle = (workers, tasks) => __awaiter(void 0, void 0, void 0, function* () {
-    const results = [];
-    const executeTask = (task) => __awaiter(void 0, void 0, void 0, function* () {
-        const result = yield task();
-        results.push(result);
-    });
-    const executeWorkers = () => __awaiter(void 0, void 0, void 0, function* () {
-        const workerPromises = [];
-        const runTask = () => __awaiter(void 0, void 0, void 0, function* () {
-            if (tasks.length > 0) {
-                const task = tasks.shift();
-                const result = yield executeTask(task);
-                return result;
-            }
-        });
-        for (let i = 0; i < workers; i++) {
-            workerPromises.push(runTask());
+'use strict'
+var __awaiter =
+    (this && this.__awaiter) ||
+    function (thisArg, _arguments, P, generator) {
+        function adopt(value) {
+            return value instanceof P
+                ? value
+                : new P(function (resolve) {
+                      resolve(value)
+                  })
         }
-        yield Promise.all(workerPromises);
-    });
-    yield executeWorkers();
-    return results;
-});
-const bootstrap = () => __awaiter(void 0, void 0, void 0, function* () {
-    logger('Starting...');
-    const start = Date.now();
-    const answers = yield throttle(5, load);
-    logger('Done in %dms', Date.now() - start);
-    logger('Answers: %O', answers);
-});
-bootstrap().catch((err) => {
-    logger('General fail: %O', err);
-});
+        return new (P || (P = Promise))(function (resolve, reject) {
+            function fulfilled(value) {
+                try {
+                    step(generator.next(value))
+                } catch (e) {
+                    reject(e)
+                }
+            }
+            function rejected(value) {
+                try {
+                    step(generator['throw'](value))
+                } catch (e) {
+                    reject(e)
+                }
+            }
+            function step(result) {
+                result.done
+                    ? resolve(result.value)
+                    : adopt(result.value).then(fulfilled, rejected)
+            }
+            step(
+                (generator = generator.apply(thisArg, _arguments || [])).next(),
+            )
+        })
+    }
+const debug = require('debug')
+const logger = debug('core')
+const delays = [...Array(50)].map(() => Math.floor(Math.random() * 900) + 100)
+const load = delays.map(
+    delay => () =>
+        new Promise(resolve => {
+            setTimeout(() => resolve(Math.floor(delay / 100)), delay)
+        }),
+)
+const throttle = (workers, tasks) =>
+    __awaiter(void 0, void 0, void 0, function* () {
+        const results = []
+        const executeTask = task =>
+            __awaiter(void 0, void 0, void 0, function* () {
+                const result = yield task()
+                results.push(result)
+            })
+        const executeWorkers = () =>
+            __awaiter(void 0, void 0, void 0, function* () {
+                const workerPromises = []
+                const runTask = () =>
+                    __awaiter(void 0, void 0, void 0, function* () {
+                        if (tasks.length > 0) {
+                            const task = tasks.shift()
+                            const result = yield executeTask(task)
+                            return result
+                        }
+                    })
+                for (let i = 0; i < workers; i++) {
+                    workerPromises.push(runTask())
+                }
+                yield Promise.all(workerPromises)
+            })
+        yield executeWorkers()
+        return results
+    })
+const bootstrap = () =>
+    __awaiter(void 0, void 0, void 0, function* () {
+        logger('Starting...')
+        const start = Date.now()
+        const answers = yield throttle(5, load)
+        logger('Done in %dms', Date.now() - start)
+        logger('Answers: %O', answers)
+    })
+bootstrap().catch(err => {
+    logger('General fail: %O', err)
+})
--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,62 @@
-const debug = require('debug');
+const debug = require('debug')
 
-const logger = debug('core');
+const logger = debug('core')
 
 interface TaskType {
     (): Promise<number>
 }
 
-const delays = [...Array(50)].map(() => Math.floor(Math.random() * 900) + 100);
+const delays = [...Array(50)].map(() => Math.floor(Math.random() * 900) + 100)
 const load: Array<() => Promise<number>> = delays.map(
-  (delay) => (): Promise<number> => new Promise((resolve) => {
-    setTimeout(() => resolve(Math.floor(delay / 100)), delay);
-  }),
-);
+    delay => (): Promise<number> =>
+        new Promise(resolve => {
+            setTimeout(() => resolve(Math.floor(delay / 100)), delay)
+        }),
+)
 
 const throttle = async (
-  workers: number,
-  tasks: TaskType[],
+    workers: number,
+    tasks: TaskType[],
 ): Promise<number[]> => {
-  const results: number[] = [];
+    const results: number[] = []
 
-  const executeTask = async (task: TaskType): Promise<void> => {
-    const result = await task();
-    results.push(result);
-  };
-
-  const executeWorkers = async (): Promise<void> => {
-    const workerPromises: Promise<void>[] = [];
-
-    const runTask = async (): Promise<number | void> => {
-      if (tasks.length > 0) {
-        const task = tasks.shift()!;
-        const result = await executeTask(task);
-
-        return result;
-      }
-    };
-
-    for (let i = 0; i < workers; i++) {
-      workerPromises.push(runTask() as Promise<void>);
+    const executeTask = async (task: TaskType): Promise<void> => {
+        const result = await task()
+        results.push(result)
     }
 
-    await Promise.all(workerPromises);
-  };
+    const executeWorkers = async (): Promise<void> => {
+        const workerPromises: Promise<void>[] = []
 
-  await executeWorkers();
+        const runTask = async (): Promise<number | void> => {
+            if (tasks.length > 0) {
+                const task = tasks.shift()!
+                const result = await executeTask(task)
 
-  return results;
-};
+                return result
+            }
+        }
+
+        for (let i = 0; i < workers; i++) {
+            workerPromises.push(runTask() as Promise<void>)
+        }
+
+        await Promise.all(workerPromises)
+    }
+
+    await executeWorkers()
+
+    return results
+}
 
 const bootstrap = async () => {
-  logger('Starting...');
-  const start = Date.now();
-  const answers = await throttle(5, load);
-  logger('Done in %dms', Date.now() - start);
-  logger('Answers: %O', answers);
-};
+    logger('Starting...')
+    const start = Date.now()
+    const answers = await throttle(5, load)
+    logger('Done in %dms', Date.now() - start)
+    logger('Answers: %O', answers)
+}
 
-bootstrap().catch((err) => {
-  logger('General fail: %O', err);
-});
+bootstrap().catch(err => {
+    logger('General fail: %O', err)
+})
